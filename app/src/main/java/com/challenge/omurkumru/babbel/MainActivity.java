1ba6b354fc99a4b62df2966c07e438a53ee8c470
omurkumru <omurkumru@outlook.com>
2018-10-24 02:19:36 +0200

    init project

---
+++ app/src/main/java/com/challenge/omurkumru/babbel/MainActivity.java
@@ -0,0 +1,13 @@
+package com.challenge.omurkumru.babbel;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
